3f92f643a968bbc7083c8fa75ebcb5fac330e97c
pepe1973 <bodrogipr@gmail.com>
2019-11-12 15:08:32 +0100

    ismét commit

--- MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
+++ MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
@@ -10,6 +10,66 @@ namespace MasodikValodiCsharpProjekt
     {
         static void Main(string[] args)
         {
+            /*Console.WriteLine("1-től 100-ig azok a számok, melyek valódi osztóinak összege osztható héttel.");
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (osszegHettel(i))
+                {
+                    Console.WriteLine($"{i} valódi osztóinak összege osztható 7-tel.");
+                }
+            }*/
+
+            Console.WriteLine("2-től 100-ig a prímszámok: ");
+
+            for (int i = 2; i < 101; i++)
+            {
+                if (primszamE(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }      
+
+            Console.ReadKey(true);
+            
+        }
+
+        static bool osszegHettel(int szam)
+        {
+            int osszeg = 0;
+            bool hettel = true;
+
+            for (int i = 1; i < szam; i++)
+            {
+                if (szam % i == 0)
+                {
+                    osszeg += i;
+                }
+            }
+
+            if (osszeg % 7 != 0)
+            {
+                hettel = false;
+            }
+
+            return hettel;
+        }
+
+        static bool primszamE(int szam)
+        {
+            bool primE = true;
+            int hatar = (int)Math.Floor(Math.Sqrt(szam)) + 1;
+
+            for (int i = 2; i < hatar; i++)
+            {
+                if (szam % i == 0)
+                {
+                    primE = false;
+                    break;
+                }
+            }
+
+            return primE;
         }
     }
 }
