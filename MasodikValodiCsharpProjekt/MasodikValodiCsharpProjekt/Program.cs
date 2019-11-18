74c3b6cff7823bc38716067726086296c8f8991c
pepe1973 <bodrogipr@gmail.com>
2019-11-18 13:11:11 +0100

    ismét mentés

--- MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
+++ MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
@@ -20,7 +20,7 @@ namespace MasodikValodiCsharpProjekt
                 }
             }*/
 
-            Console.WriteLine("2-től 100-ig a prímszámok: ");
+            /*Console.WriteLine("2-től 100-ig a prímszámok: ");
 
             for (int i = 2; i < 101; i++)
             {
@@ -28,7 +28,127 @@ namespace MasodikValodiCsharpProjekt
                 {
                     Console.WriteLine(i);
                 }
-            }      
+            }*/
+
+            /* Int32.TryParse használata 
+            int szam = -89;
+            bool egeszSzam = true;
+
+            do
+            {
+                Console.Write("Kérek egy számot: ");
+                egeszSzam = Int32.TryParse(Console.ReadLine(), out szam);
+            } while (!egeszSzam);
+
+            Console.WriteLine($"A bekért szám: {szam}");*/
+
+            /* 23. feladat: 
+            string kimenet = "";
+
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            kimenet += szam + " = ";
+
+            while (szam % 2 == 0)
+            {
+                kimenet += 2 + " * ";
+                szam = szam / 2;
+            }
+
+            kimenet += szam;
+
+            Console.WriteLine(kimenet); */
+
+            /* 24. feladat:
+            string alma = "";
+
+            while (alma != "alma")
+            {
+                Console.Write("Kérek egy gyümölcsöt: ");
+                alma = Console.ReadLine();
+            }
+
+            Console.WriteLine("Az alma gyümölcs!");*/
+
+            /* 25. feladat:
+            string kimenet = "";
+
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            kimenet += szam + " = ";
+
+            int egesz = 0;
+            int tort = szam;
+
+            while (szam - 3 >= 0)
+            {
+                szam = szam - 3;
+                egesz++;
+                tort = szam;
+            }
+
+            kimenet += egesz + " * 3 + " + tort;
+
+            Console.WriteLine(kimenet); */
+
+            /* 28. - 29. feladat:
+            string kimenet = "";
+
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            kimenet += szam + " = ";
+
+            int meddig = (int)(Math.Floor(Math.Sqrt(szam))) + 1;
+
+            for (int i = 2; i < meddig; i++)
+            {
+                if (szam % i == 0)
+                {
+                    if (szam == i)
+                    {
+                        kimenet += i;
+                    }
+                    else
+                    {
+                        kimenet += i + " * ";
+                        szam = szam / i;
+                        i--;
+                    } 
+                }
+            }
+
+            Console.WriteLine(kimenet);*/
+
+            /* 32. feladat: 
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < szam +1; i++)
+            {
+                for (int j = 1; j < szam + 1; j++)
+                {
+                    Console.SetCursorPosition(3 * (j - 1) + 1, i);
+                    Console.Write($"{i * j} ");
+                }
+                Console.WriteLine();
+            }*/
+
+            // 34. feladat:
+            for (int i = 10; i < 100; i++)
+            {
+                for (int j = 10; j < 100; j++)
+                {
+
+                }
+
+                for (int k = 10; k < 100; k++)
+                {
+
+                }
+            }
 
             Console.ReadKey(true);
             
---
+++ RefAndOut/RefAndOut/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem az első számot: ");
+            int elso = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem a második számot: ");
+            int masodik = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérek egy nevet: ");
+            string nev = Console.ReadLine();
+
+            Console.WriteLine($"A két szám összege: {Osszead(out elso, out masodik, out nev)}");
+            Console.WriteLine($"A két szám: {elso}, {masodik}");
+            Console.WriteLine($"A név: {nev}");
+
+            Console.ReadKey(true);
+        }
+
+        /*static int Osszead(int a, int b)
+        {
+            
+            // Létrehozom  visszatérő értéket és inicializálom.
+            int osszeg = 0;
+            
+            // Kiszámolom a visszatérő értéket.
+            osszeg = a + b;
+
+            // Visszaadom a visszatérő értéket.
+            return osszeg;
+            
+            int osszeg = a + b;
+
+            a = 128;
+            b = -49;
+            return osszeg;
+        }*/
+
+        static int Osszead(out int a, out int b, out string nev)
+        {
+            a = 0;
+            b = 0;
+            int osszeg = a + b;
+
+            //a = 128;
+            //b = -49;
+            nev = "Gábor";
+            return osszeg;
+        }
+    }
+}
