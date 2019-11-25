724ff9ea73822d42df63e13ccef8c37a3672721a
pepe1973 <bodrogipr@gmail.com>
2019-11-25 08:51:00 +0100

    megint

--- MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
+++ MasodikValodiCsharpProjekt/MasodikValodiCsharpProjekt/Program.cs
@@ -157,7 +157,7 @@ namespace MasodikValodiCsharpProjekt
                 }
             }*/
 
-            // 35. feladat:
+            /* 35. feladat:
             int sorok = 3;
             for (int i = 0; i < sorok; i++)
             {
@@ -170,6 +170,55 @@ namespace MasodikValodiCsharpProjekt
                         Console.WriteLine($"{(char)(eddig)} {eddig}");
                     }
                 }
+            }*/
+
+            /* 36. feladat:
+            Console.Write("Kérem a sorok számát: ");
+            int sorok = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem az oszlopok számát: ");
+            int oszlopok = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write('x');
+                    }
+                    else
+                    {
+                        Console.Write('o');
+                    }
+                }
+                Console.WriteLine();
+            }*/
+
+            /* 37. feladat:
+            Console.Write("Kérem  sorok számát: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }*/
+
+            // 38. feladat:
+            Console.Write("Kérem  sorok számát: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < a; i++)
+            {
+                Console.SetCursorPosition(a - i, i + 1);
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
             }
 
             Console.ReadKey(true);
