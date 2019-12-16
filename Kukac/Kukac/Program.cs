9aee55665c0a02165983589b22967c2480559534
pepe1973 <bodrogipr@gmail.com>
2019-12-16 13:04:24 +0100

    ismét változik

--- Kukac/Kukac/Program.cs
+++ Kukac/Kukac/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace Ismetles2
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
             int szelesseg = Console.WindowWidth;
             int magassag = Console.WindowHeight;
             int x = szelesseg / 2;
@@ -18,18 +19,25 @@ namespace Ismetles2
             int[] xCoord = new int[5];
             int[] yCoord = new int[5];
 
+            int[] pontokX = new int[10];
+            int[] pontokY = new int[10];
+
             for (int i = 0; i < 5; i++)
             {
                 xCoord[i] = x + i;
                 yCoord[i] = y;
             }
 
+            for (int i = 0; i < 10; i++)
+            {
+                pontokX[i] = rnd.Next(0, szelesseg);
+                pontokY[i] = rnd.Next(0, magassag);
+            }
 
+            Megrajzol(pontokX, pontokY);
             Megrajzol(xCoord, yCoord);
            
-
             //Console.WriteLine($"{szelesseg} és {magassag}");
-
 
             ConsoleKeyInfo cki = new ConsoleKeyInfo();
 
@@ -38,26 +46,61 @@ namespace Ismetles2
                 cki = Console.ReadKey();
                 switch (cki.Key)
                 {
-                    
                     case ConsoleKey.LeftArrow:
                         Console.Clear();
+                        Megrajzol(pontokX, pontokY);
                         Balfele(ref xCoord, ref yCoord);
                         Megrajzol(xCoord, yCoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xCoord[xCoord.Length - 1] && pontokY[i] == yCoord[yCoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                     case ConsoleKey.UpArrow:
                         Console.Clear();
+                        Megrajzol(pontokX, pontokY);
                         Felfele(ref xCoord, ref yCoord);
                         Megrajzol(xCoord, yCoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xCoord[xCoord.Length - 1] && pontokY[i] == yCoord[yCoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                     case ConsoleKey.RightArrow:
                         Console.Clear();
+                        Megrajzol(pontokX, pontokY);
                         Jobbfele(ref xCoord, ref yCoord);
                         Megrajzol(xCoord, yCoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xCoord[xCoord.Length - 1] && pontokY[i] == yCoord[yCoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                     case ConsoleKey.DownArrow:
                         Console.Clear();
+                        Megrajzol(pontokX, pontokY);
                         Lefele(ref xCoord, ref yCoord);
                         Megrajzol(xCoord, yCoord);
+                        for (int i = 0; i < pontokX.Length; i++)
+                        {
+                            if (pontokX[i] == xCoord[xCoord.Length - 1] && pontokY[i] == yCoord[yCoord.Length - 1])
+                            {
+                                pontokX = EggyelCsokkent(pontokX[i], pontokX);
+                                pontokY = EggyelCsokkent(pontokY[i], pontokY);
+                            }
+                        }
                         break;
                    
                 }
@@ -68,9 +111,8 @@ namespace Ismetles2
 
         static void Megrajzol(int[] x, int[] y)
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < x.Length; i++)
             {
-                
                 Console.SetCursorPosition(x[i], y[i]);
                 Console.Write("*");
             }
@@ -124,5 +166,18 @@ namespace Ismetles2
             x[4] = atmenetX[4] - 1;
             y[4] = atmenetY[4];
         }
+
+        static int[] EggyelCsokkent(int szam, int[] a)
+        {
+            int hossz = a.Length;
+            int[] b = new int[hossz - 1];
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                    b[i] = a[i];
+            }
+
+            return b;
+        } 
     }
 }
---
+++ kukacRoviden/kukacRoviden/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kukacRoviden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szelesseg = Console.WindowWidth;
+            int magassag = Console.WindowHeight;
+            int x = szelesseg / 2;
+            int y = magassag / 2;
+
+            int[] xCoord = new int[5];
+            int[] yCoord = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                xCoord[i] = x + i;
+                yCoord[i] = y;
+            }
+
+            Megrajzol(xCoord, yCoord);
+
+            //Console.WriteLine($"{szelesseg} és {magassag}");
+
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+
+            while (cki.Key != ConsoleKey.Escape)
+            {
+                cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        Console.Clear();
+                        Mozgat('b', ref xCoord, ref yCoord);
+                        Megrajzol(xCoord, yCoord);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.Clear();
+                        Mozgat('f', ref xCoord, ref yCoord);
+                        Megrajzol(xCoord, yCoord);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Clear();
+                        Mozgat('j', ref xCoord, ref yCoord);
+                        Megrajzol(xCoord, yCoord);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.Clear();
+                        Mozgat('l', ref xCoord, ref yCoord);
+                        Megrajzol(xCoord, yCoord);
+                        break;
+
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+
+        static void Megrajzol(int[] x, int[] y)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.SetCursorPosition(x[i], y[i]);
+                Console.Write("*");
+            }
+        }
+        static void Mozgat(char merre, ref int[] x, ref int[] y)
+        {
+            int[] atmenetX = x;
+            int[] atmenetY = y;
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = atmenetX[i + 1];
+                y[i] = atmenetY[i + 1];
+            }
+            switch (merre)
+            {
+                case 'f':
+                    x[4] = atmenetX[4];
+                    y[4] = atmenetY[4] - 1;
+                    break;
+                case 'l':
+                    x[4] = atmenetX[4];
+                    y[4] = atmenetY[4] + 1;
+                    break;
+                case 'b':
+                    x[4] = atmenetX[4] - 1;
+                    y[4] = atmenetY[4];
+                    break;
+                case 'j':
+                    x[4] = atmenetX[4] + 1;
+                    y[4] = atmenetY[4];
+                    break;
+            }
+        }
+    }
+}
