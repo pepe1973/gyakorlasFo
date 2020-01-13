96dfc064c4790bc4566b6ff7c3def12e62c024e6
bodrogi.peter.r <bodrogi.peter.r@O16TANAR>
2020-01-13 12:55:27 +0100

    ismet kommit

---
+++ OsztalyokKetto/OsztalyokKetto/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsztalyokKetto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Megye[] megyek = new Megye[4];
+            for (int i = 0; i < megyek.Length; i++)
+            {
+                Console.Write("Kérem a megye nevét: ");
+                string nev = Console.ReadLine();
+                Console.Write("Kérem a megye területét: ");
+                int terulet = Int32.Parse(Console.ReadLine());
+                Console.Write("Kérem a megye székhelyét: ");
+                string megyeSzekhely = Console.ReadLine();
+                Console.Write("Kelet-Magyarországi-e: ");
+                bool keletiE = Boolean.Parse(Console.ReadLine());
+                megyek[i] = new Megye(nev, terulet, megyeSzekhely, keletiE);
+            }
+
+            for (int i = 0; i < megyek.Length; i++)
+            {
+                megyek[i].KiirAdatok();
+            }
+            
+
+            /*Megye megye = new Megye(nev, terulet, megyeSzekhely);
+            megye.KiirAdatok();*/
+
+            /*NyugatMagyarorszagiMegye nyMegye = new NyugatMagyarorszagiMegye(nev, terulet, megyeSzekhely, nyugatiE);
+            nyMegye.KiirAdatok();*/
+                
+            Console.ReadKey(true);
+        }
+    }
+
+    class Megye
+    {
+        public string Nev { get; set; }
+        public int Terulet { get; set; }
+        public string MegyeSzekhely { get; set; }
+        public bool KeletMagyarorszagiE { get; set; }
+
+        public Megye(string nev, int terulet, string megyeSzekhely)
+        {
+            this.Nev = nev;
+            this.Terulet = terulet;
+            this.MegyeSzekhely = megyeSzekhely;
+        }
+
+        public Megye(string nev, int terulet, string megyeSzekhely, bool keletiE) : this(nev, terulet, megyeSzekhely)
+        {
+            this.KeletMagyarorszagiE = keletiE;
+        }
+
+        public void KiirAdatok()
+        {
+            if (this.KeletMagyarorszagiE)
+            {
+                Console.WriteLine($"A megye neve: {this.Nev} megye, területe: {this.Terulet} és megye székhelye: {this.MegyeSzekhely}.");
+            }
+        }
+    }
+
+    class NyugatMagyarorszagiMegye : Megye
+    {
+        public bool NyugatMagyarorszagiE { get; set; }
+
+        public NyugatMagyarorszagiMegye(string nev, int terulet, string megyeSzekhely, bool nyugatiE) : base(nev, terulet, megyeSzekhely)
+        {
+            this.NyugatMagyarorszagiE = nyugatiE;
+        }
+
+        new public void KiirAdatok()
+        {
+            if (this.NyugatMagyarorszagiE)
+            {
+                Console.WriteLine($"A megye neve: {this.Nev} megye, területe: {this.Terulet} és megye székhelye: {this.MegyeSzekhely}.");
+            }
+        }
+    }
+}
---
+++ Sikidomok/Sikidomok/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sikidomok
+{
+    class Program
+    {
+        static void Kiiras(Negyzet negy)
+        {
+            if (negy is Negyzet)
+            {
+                Console.WriteLine($"A negyzet kerülete: {(negy as Negyzet).KeruletSzamol()}");
+            }
+            else if (negy is Teglalap)
+            {
+                Console.WriteLine($"A negyzet kerülete: {(negy as Teglalap).KeruletSzamol()}");
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            /*Negyzet negyzet = new Negyzet(12);
+            Console.WriteLine($"A négyzet kerülete: {negyzet.KeruletSzamol()}");
+            Console.WriteLine($"A négyzet területe: {negyzet.TeruletSzamol()}");
+
+            Teglalap teglalap = new Teglalap(12, 10);
+            Console.WriteLine($"A téglalap kerülete: {teglalap.KeruletSzamol()}");
+            Console.WriteLine($"A téglalap területe: {teglalap.TeruletSzamol()}");*/
+
+            Negyzet negyzet = new Negyzet(10);
+            Teglalap teglalap = new Teglalap(20, 30);
+
+            Kiiras(negyzet);
+            Kiiras(teglalap);
+
+            Console.ReadKey(true);
+        }
+    }
+
+    class Negyzet
+    {
+        public int AOldal { get; set; }
+
+        public Negyzet()
+        {
+
+        }
+
+        public Negyzet(int aoldal)
+        {
+            this.AOldal = aoldal;
+        }
+
+        public int TeruletSzamol()
+        {
+            return this.AOldal * this.AOldal;
+        }
+
+        public int KeruletSzamol()
+        {
+            return 4 * this.AOldal;
+        }
+
+        public void Kiir(Negyzet negy)
+        {
+            Console.WriteLine($"A negyzet kerülete: {negy.KeruletSzamol()}");
+        }
+    }
+
+    class Teglalap : Negyzet
+    {
+        public int BOldal { get; set; }
+
+        public Teglalap(int aOldal, int bOldal) : base(aOldal)
+        {
+            this.BOldal = bOldal;
+        }
+
+        public Teglalap()
+        {
+
+        }
+
+        new public int TeruletSzamol()
+        {
+            return this.AOldal * this.BOldal;
+        }
+
+        new public int KeruletSzamol()
+        {
+            return 2 * (this.AOldal + this.BOldal);
+        }
+
+        new public void Kiir(Negyzet negy)
+        {
+            Console.WriteLine($"A negyzet kerülete: {negy.KeruletSzamol()}");
+        }
+    }
+}
